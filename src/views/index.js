8be0b61d1b12ea8a96d0417d4448eab6d0b39a86
maray29 <ayk.martirosyan@gmail.com>
2023-04-24 22:37:05 +0200

    Implement barbajs transitions.

--- src/views/index.js
+++ src/views/index.js
@@ -1,34 +1,19 @@
 import gsap from 'gsap'
 import PlaneHover from '../PlaneHover.js'
 import { restartWebflow } from '@finsweet/ts-utils'
+import HomePageAnimation from '../animation/HomePageAnimation.js'
 
 export const index = {
   namespace: 'home',
 
   beforeEnter() {
-    // this.program.bodyHeight = document.body.scrollHeight
-    // this.createLenis()
-    const projectsImages = document.querySelectorAll('.projects_list img')
     const projectsList = document.querySelector('.projects_list')
+    // Move this tween to transitions?
     gsap.to(APP.stage.container, {
       opacity: 1,
     })
     if (!APP.plane) {
       APP.plane = new PlaneHover(projectsList, APP.stage, { strength: 0.35, transparent: true })
     }
-  },
-
-  beforeLeave() {
-    // Do smth
-  },
-
-  afterEnter() {
-    console.log('hello')
-    // APP.homePageAnimation.animateProjectHeadings()
-
-    // const projectsList = document.querySelector('.projects_list')
-    // APP.plane = new PlaneHover(projectsList, APP.stage, { strength: 0.35, transparent: true })
-    // APP.createLenis()
-    // console.log('ADDING PLANE AFTER')
   },
 }
